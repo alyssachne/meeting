db5bd92ee652c13456d9a8f6671f9a04370cb49f
yifan <yvan.qu@mail.utoronto.ca>
2020-11-11 22:26:19 +0800

    add messaging in Speaker but unfinished

--- phase1/src/Entity/Speaker.java
+++ phase1/src/Entity/Speaker.java
@@ -1,10 +1,22 @@
 package Entity;
 
+import java.util.ArrayList;
+
 public class Speaker extends User {
+    private ArrayList<Event> speaks;
 
     public Speaker(String name, String username, String password) {
         super(name, username, password);
     }
+
+    public void addSpeak(Event speak){
+        this.speaks.add(speak);
+    }
+
+    public ArrayList<Event> getSpeaks() {
+        return speaks;
+    }
+
 
     @Override
     public String typeGetter(){return "Speaker";};
--- phase1/src/Usecase/SpeakerAct.java
+++ phase1/src/Usecase/SpeakerAct.java
@@ -1,9 +1,38 @@
 package Usecase;
 
-import Entity.Speaker;
+import Entity.*;
+import org.omg.CORBA.Any;
+
+import java.util.ArrayList;
 
 public class SpeakerAct {
-    public void createSpeaker(String name, String username, String password){
+    public void createSpeaker(String name, String username, String password, ArrayList<Event> speaks){
+        Speaker speaker = new Speaker(name, username, password);
+    }
+    public boolean sentMessageToAttendee(Speaker speaker, ArrayList<Event> speaks, Message message){
+        // Exception needed here
+        speaker.sendMessage(message);
+        for (Event speak : speaks){
+            if (speaker.getSpeaks().contains(speak)){
+                return false;
+            }
+            for (String user :speak.ListOfAttendees){
+                // user receive messages
+
+            }
+        }
+        return true;
+    }
+
+//    private void sentMessage(User user, ArrayList<User> receivers, Message message){
+//        //Can we import Userlist from UserOragnizer(Use case)？
+//        for (User receiver: receivers){
+//            receiver.receiveMessage(message);
+//        }
+//        user.sendMessage(message);
+//    }
+
+    public void createSpeaker(String name, String username, String password) {
         Speaker speaker = new Speaker(name, username, password);
     }
 }
