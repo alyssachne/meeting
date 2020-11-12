23e9b66d8f6101fa67851b838b4f6280f93043c7
HYF66 <Qiwpyq-napme0-senvyv>
2020-11-12 23:22:51 +0800

    Taken two modes of message sending into consideration. 1. Send group messages to existing talk. 2. Send message to specific users.

--- phase1/src/Presenter/SpeakerPresenter.java
+++ phase1/src/Presenter/SpeakerPresenter.java
@@ -4,7 +4,7 @@ public class SpeakerPresenter {
     public void mainMenu(){
         System.out.println("Please enter your choice below:");
         System.out.println("1.Set title of a speech");
-        System.out.println("3.Send a message");
+        System.out.println("2.Send a message");
     }
 
     public void setTitle(){
@@ -12,8 +12,19 @@ public class SpeakerPresenter {
         System.out.println("Press Enter button after your entry");
     }
 
-    public void sendMessage(){
-        System.out.println("Please input your message below");
+    public void receiver1(){
+        System.out.println("Please select the group of receivers you want to message to");
+        System.out.println("I want to send message to all the attendees from my talk(s)");
+        System.out.println("I want to specify receivers myself.");
+    }
+
+    public void receiver2(){
+        System.out.println("Please input the talk id of the talk you want to send group message to.");
+    }
+
+    public void receiver3(){
+        System.out.println("Please put below the user name(s) of the users you want to send meaasages to, if you want " +
+                "to send messages to multiple users, please separate the usernames with comma");
     }
 }
 
