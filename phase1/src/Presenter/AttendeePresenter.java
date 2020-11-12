370977edc2a180a468f89a5dc502f08eb6498d18
Ertong Wei <ertong.wei@mail.utoronto.ca>
2020-11-12 23:21:58 +0800

    Ertong Wei

--- phase1/src/Presenter/AttendeePresenter.java
+++ phase1/src/Presenter/AttendeePresenter.java
@@ -3,19 +3,26 @@ package Presenter;
 public class AttendeePresenter {
     public void mainMenu(){
         System.out.println("Please enter your choice below:");
-        System.out.println("1.Log in");
-        System.out.println("2.Exist");
+        System.out.println("1.See the schedule of events you can sign up");
+        System.out.println("2.See the schedule of events you signed up");
+        System.out.println("3.Send messages");
+        System.out.println("4.Check mailbox");
     }
 
-    public void eventMenu(){
-        System.out.println("Please enter your choice below:");
-        System.out.println("1.Sign up an event");
-        System.out.println("2.Cancel an event");
+    public void eventSignUpMenu(){
+        System.out.println("Please enter the event name you want to sign up");
     }
 
-    public void messageMenu(){
+    public void eventCancelMenu(){
+        System.out.println("Please enter the event name you want to cancel");
+    }
+
+    public void sendMessagesMenu(){
         System.out.println("Please enter the receiver name");
-        System.out.println("Please enter the message");
+        System.out.println("Please enter message");
     }
 
+    public void checkMailbox(){
+        System.out.println("Please enter the sender name");
+    }
 }
