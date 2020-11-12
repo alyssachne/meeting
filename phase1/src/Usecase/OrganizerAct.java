2434c468b53e9763b3f837c4c745b708816b850c
HYF66 <Qiwpyq-napme0-senvyv>
2020-11-12 10:37:27 +0800

    First draft

--- phase1/src/Usecase/OrganizerAct.java
+++ phase1/src/Usecase/OrganizerAct.java
@@ -6,10 +6,51 @@ import java.util.ArrayList;
 
 public class OrganizerAct {
 
-    public void createOrganizer(String name, String username, String password){
-        Organizer organizer = new Organizer(name, username, password);
+//    public void createOrganizer(String name, String username, String password){
+//        Organizer organizer = new Organizer(name, username, password);
+//    }
+
+    public void creatUser(String name, String Username, String password, String type){
+        if (type.equals('Attendee')){
+            new Attendee();
+        }
+        if(type.equals('Speaker')){
+            new Speaker();
+        }
+        if (type.equals('Organizer')){
+            new Organizer();
+        }
+        else{
+            new User();
+        }
+    }
+    //user 没有写对应的构造函数，子类无法调用父类构造函数，在使用子类无参构造函数时可能遇到问题
+    //use needs to include inbox and outbox in the constructor
+
+    public boolean setSpeaker(Event talk, Speaker speaker){
+        talk.setSpeaker(speaker);
+        return True;
+    //event needs one instance: speaker
     }
 
+    public boolean setTime(Event talk, Calendar time){
+        talk.setTime(time);
+        return Ture;
+    }
+
+    public boolean setLocation(Event talk, int roomId){
+        talk.setRoom(roomId);
+        return True;
+    }
+
+    public boolean sendMessage(ArrayList<User> receiver, Object content){
+        newMessage = new Message(this,receiver,content);
+        this.outbox.add(newMessage);
+        for (int i = 0; i < receiver.length; i++){
+            receiver[i].inbox.add(newMessage);
+        }
+        return True;
+    }
 //    public void createSpeaker(String name, String username, String password){
 //        Speaker speaker = new Speaker(name, username, password);
 //    }
