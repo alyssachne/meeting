85d35ee04e7e0cabbec0bdf7791a27a08a1cf46d
hongyuanhua <hhy981123@gmail.com>
2020-11-11 23:19:34 -0500

    OrganizerPresenter tmp added, Demo modified

--- phase1/src/Presenter/OrganizerPresenter.java
+++ phase1/src/Presenter/OrganizerPresenter.java
@@ -1,4 +1,22 @@
 package Presenter;
 
 public class OrganizerPresenter {
+    public void mainMenu(){
+        System.out.println("Please enter your choice below:");
+        System.out.println("1.Create Rooms");
+        System.out.println("2.Create a Speaker Account");
+        System.out.println("3.Send a message");
+    }
+
+    public void roomMenu(){
+        System.out.println("Please enter the room id and max capacity of the room:");
+        System.out.println("Press Enter button after each entry");
+    }
+
+    public void speakerMenu(){
+        //Create speaker and assign speaker to rooms
+        System.out.println("Please ");
+    }
+
+
 }
--- phase1/src/UI/Demo.java
+++ phase1/src/UI/Demo.java
@@ -1,12 +1,14 @@
 package UI;
 
-import Usecase.UserOrganizer;
+import Controller.UserController;
+import Entity.Organizer;
+import Presenter.OrganizerPresenter;
 
 import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
-        UserOrganizer uo = new UserOrganizer();
+        UserController uo = new UserController();
         uo.createOrganizer();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your username:");
@@ -14,7 +16,28 @@ public class Demo {
 //        System.out.println("Username is: " + username);
         System.out.println("Please enter your password:");
         String password = scanner.nextLine();
-        System.out.println(uo.Login(username,password));
+        System.out.println(uo.Login(username, password));
 
+        if (uo.Login(username, password) && uo.userType().equals("Organizer")) {
+            OrganizerPresenter op = new OrganizerPresenter();
+            op.mainMenu();
+            String choice = scanner.nextLine();
+            if (choice.equals("1")) {
+                op.roomMenu();
+                String id = scanner.nextLine();
+                String MaxCapacity = scanner.nextLine();
+                //create the room
+            } else if (choice.equals("2")) {
+                //speaker
+            } else if (choice.equals("3")) {
+                //message
+            }
+        } else if (uo.Login(username, password) && uo.userType().equals("Speaker")) {
+            //call Speaker Presenter
+        } else if (uo.Login(username, password) && uo.userType().equals("Attendee")) {
+            //call Attendee Presenter
+        } else {
+            System.out.println("The username or password does not match!");
+        }
     }
 }
