807949d065f54d7f8749aa92428b03740dd10acb
HYF66 <Qiwpyq-napme0-senvyv>
2020-11-12 19:15:49 +0800

    First draft of speaker presenter.

--- phase1/src/Presenter/SpeakerPresenter.java
+++ phase1/src/Presenter/SpeakerPresenter.java
@@ -1,5 +1,19 @@
 package Presenter;
 
 public class SpeakerPresenter {
+    public void mainMenu(){
+        System.out.println("Please enter your choice below:");
+        System.out.println("1.Set title of a speech");
+        System.out.println("3.Send a message");
+    }
 
+    public void setTitle(){
+        System.out.println("Please enter the title of the speech:");
+        System.out.println("Press Enter button after your entry");
+    }
+
+    public void sendMessage(){
+        System.out.println("Please input your message below");
+    }
 }
+
