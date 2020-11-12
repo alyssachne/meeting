5f43851b84ebdc2408386bc256be4c670513e03f
cheny845 <yuqi.chen@mail.utoronto.ca>
2020-11-12 15:12:24 -0500

    modified user

--- phase1/src/Entity/User.java
+++ phase1/src/Entity/User.java
@@ -1,21 +1,28 @@
 package Entity;
 
 import Usecase.MessageHistory;
+import org.omg.PortableInterceptor.INACTIVE;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class User {
     private String name;
     private String username;
     private String password;
-    private MessageBox InBox;
-    private MessageBox SentBox;
-    private HashMap<String, MessageHistory> messageList = new HashMap<>();
+    //private MessageBox InBox;
+    //private MessageBox SentBox;
+    //private HashMap<String, MessageHistory> messageList = new HashMap<>();
+    public ArrayList<Integer> eventList;
+    public ArrayList<String> contacts;
 
-    public User(String name, String username, String password){
+    public User(String name, String username, String password, ArrayList<Integer> eventList,
+                ArrayList<String> contacts){
         this.name = name;
         this.username = username;
         this.password = password;
+        this.eventList = eventList;
+        this.contacts = contacts;
     }
 
     public String getName(){
@@ -42,17 +49,49 @@ public class User {
         this.password = password;
     }
 
-    public void sendMessage(Message message){
-        this.SentBox.addMessage(message);
+    public ArrayList<Integer> getEventList() {
+        return eventList;
     }
 
-    public void receiveMessage(Message message){
-        this.InBox.addMessage(message);
+    public ArrayList<String> getContacts() {
+        return contacts;
     }
 
-    public void viewMessageIn() {
-        System.out.println(this.InBox);
+    public boolean signUp(Integer eventId) {
+        if(eventList.contains(eventId)) {
+            return false;
+        }
+        eventList.add(eventId);
+        return true;
     }
+
+    public boolean cancelSpot(Integer eventId) {
+        return eventList.remove(eventId);
+    }
+
+    public boolean addContact(String username) {
+        if(contacts.contains(username)) {
+            return false;
+        }
+        contacts.add(username);
+        return true;
+    }
+
+    public boolean removeContact(String username) {
+        return contacts.remove(username);
+    }
+
+//    public void sendMessage(Message message){
+//        this.SentBox.addMessage(message);
+//    }
+//
+//    public void receiveMessage(Message message){
+//        this.InBox.addMessage(message);
+//    }
+//
+//    public void viewMessageIn() {
+//        System.out.println(this.InBox);
+//    }
 
     public String typeGetter(){return "User";};
 
