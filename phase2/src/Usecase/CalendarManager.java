c613ab3f9295e39f42e76ae6fd10e7bbc0d40f07
Sarah He <sarahh.he@mail.utoronto.ca>
2020-12-11 17:22:28 -0500

    CalendarManager and EventFactory

--- phase2/src/Usecase/CalendarManager.java
+++ phase2/src/Usecase/CalendarManager.java
@@ -11,6 +11,9 @@ public class CalendarManager implements Serializable {
 
     public ArrayList<Calendar> allCalendars;
 
+    /**
+     * Initialize a CalendarManager
+     */
     public CalendarManager() {
         allCalendars = new ArrayList<>();
     }
@@ -20,6 +23,12 @@ public class CalendarManager implements Serializable {
 //    }
     // when check the calendar, if the date has no event (every room and speaker are free at that day), create a new
     // calendar so that organizer can schedule a new event on that day.
+
+    /**
+     * Get the Calendar
+     * @param date: the date of Calendar
+     * @return the whole calendar
+     */
     public Calendar getCalendar(Date date) {
         for (Calendar c : allCalendars) {
             if (c.getDate().equals(date)) {
@@ -31,8 +40,13 @@ public class CalendarManager implements Serializable {
         return c;
     }
 
-    // given the selected date and events happen in that date, we can tell when the room, or the speaker (depends on
-    // which class call this method in controller) are available.
+    /**
+     * Given the selected date and events happen in that date, we can tell when the room, or the speaker (depends on
+     * which class call this method in controller) are available.
+     * @param date
+     * @param events
+     * @return a List of time that is available
+     */
     public List<Integer> getAvailable(Date date, List<Integer> events) {
         List<Integer> available = new ArrayList<>();
         for (Integer time: getCalendar(date).getSchedule().keySet()) {
@@ -45,10 +59,22 @@ public class CalendarManager implements Serializable {
         return available;
     }
 
+    /**
+     * Create a new event and add into the calendar
+     * @param date: the date of the event
+     * @param time: the time of the Event
+     * @param eventId: the Id of the Event
+     */
     public void newEvent (Date date, Integer time, int eventId) {
         getCalendar(date).getSchedule().get(time).add(eventId);
     }
 
+    /**
+     * Cancel a event from the Calendar
+     * @param date: the date of the Event
+     * @param time: the time the Event start
+     * @param eventId: the Id of the Event
+     */
     public void cancelEvent (Date date, Integer time, int eventId) {
         getCalendar(date).getSchedule().get(time).remove(eventId);
     }
