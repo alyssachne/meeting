6b8f3b7ba9a4bf91e20c1ecf1a985e5b8e017435
maxinyi <xinyi.ma@mail.utoronto.ca>
2020-12-11 22:42:28 -0500

    UserPrinter javadoc

--- phase2/src/Controller/UserPrinter.java
+++ phase2/src/Controller/UserPrinter.java
@@ -4,11 +4,16 @@ import Usecase.*;
 
 import java.io.Serializable;
 
+/**
+ * This class check and print information of the user,
+ */
 public class UserPrinter implements Serializable {
 
-
     /**
-     * Printout all usernames of speakers who give the events this attendee signed up for.
+     * Print out all usernames of speakers who give the events this attendee signed up for.
+     * @param af ActFactory in Use case.
+     * @param username The username of the speakers.
+     * @param ef EventFactory in Use case.
      */
     public static void checkSpeakers(ActFactory af, String username, EventFactory ef) {
         for(Integer eventId: af.getEvents(username)) {
@@ -17,8 +22,9 @@ public class UserPrinter implements Serializable {
     }
 
     /**
-     * Printout all usernames of attendees who attend this event.
+     * Print out all usernames of attendees who attend this event.
      * @param eventId The Id of the certain event that is being chosen.
+     * @param em EventFactory in Use case.
      */
     public static void checkAudiences(Integer eventId, EventFactory em){
         for(String name : em.getEvent(eventId).getAttendees()){
@@ -27,7 +33,10 @@ public class UserPrinter implements Serializable {
     }
 
     /**
-     * printout all usernames of users who had sent messages to this user.
+     * Print out all usernames of users who had sent messages to this user.
+     * @param mm MessageManager in Use case.
+     * @param username The username of the user.
+     * @param box The message box of the user.
      */
     public static void checkContacts(MessageManager mm, String username, String box) {
         if (box.equals("all")) {
@@ -42,7 +51,9 @@ public class UserPrinter implements Serializable {
     }
 
     /**
-     * printout all usernames of users in the system.
+     * Print out all usernames of users in the system.
+     * @param af ActFactory in Use case.
+     * @param username The username of the user.
      */
     public static void checkUsers(ActFactory af, String username) {
         for (String name : af.userHashMap.keySet()) {
@@ -53,10 +64,10 @@ public class UserPrinter implements Serializable {
     }
 
     /**
-     * Change user's access to VIP
-     * @param username: the username of the User
-     * @param af: Act Factory
-     * @param access: the access level of the User
+     * Change user's access to VIP.
+     * @param username The username of the User.
+     * @param af ActFactory in Use case.
+     * @param access The access level of the User (VIP or Normal).
      */
     public static void changeUserAccess(String username, ActFactory af, String access) {
         af.changeAccess(username, access);
